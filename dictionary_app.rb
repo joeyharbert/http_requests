11f9627b0e0ef784c674a05815f083cf95a5d370
joeyharbert <joey.harbert@gmail.com>
2019-02-21 20:49:40 -0600

    adds functionality for casing user errors as well as functionality to display multiple definitions

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -2,14 +2,25 @@ require 'http'
 
 puts "Welcome to Dictionary dot you're welcome. What do ya want to know bout?"
 
-query = gets.chomp
+query = gets.chomp.downcase
 
 info = HTTP.get("https://api.wordnik.com/v4/word.json/#{query}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=APIKEY")
 
 word = info.parse[0]["word"]
 
-definition = info.parse[0]["text"]
+definition = []
+
+info.parse.each do |define|
+  definition << define["text"]
+end
+
 
 part_of_speech = info.parse[0]["partOfSpeech"]
 
-puts "#{word} (#{part_of_speech}) - #{definition}"
+puts "#{word} (#{part_of_speech}):"
+
+i = 1
+definition.each do |text|
+  puts "#{i}. #{text}"
+  i += 1
+end
