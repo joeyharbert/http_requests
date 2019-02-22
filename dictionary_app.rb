1e5dccfd924a8add9a2a0ff028fd3974e1ff79cc
joeyharbert <joey.harbert@gmail.com>
2019-02-21 21:13:06 -0600

    Adds example functionality

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -1,21 +1,25 @@
 require 'http'
 
+apikey = ""
+
 puts "Welcome to Dictionary dot you're welcome. What do ya want to know bout?"
 
 query = gets.chomp.downcase
 
-info = HTTP.get("https://api.wordnik.com/v4/word.json/#{query}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=APIKEY")
+definition_info = HTTP.get("https://api.wordnik.com/v4/word.json/#{query}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{apikey}")
 
-word = info.parse[0]["word"]
+example_info = HTTP.get("https://api.wordnik.com/v4/word.json/#{query}/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=#{apikey}")
+
+word = definition_info.parse[0]["word"]
 
 definition = []
 
-info.parse.each do |define|
+definition_info.parse.each do |define|
   definition << define["text"]
 end
 
 
-part_of_speech = info.parse[0]["partOfSpeech"]
+part_of_speech = definition_info.parse[0]["partOfSpeech"]
 
 puts "#{word} (#{part_of_speech}):"
 
@@ -23,4 +27,23 @@ i = 1
 definition.each do |text|
   puts "#{i}. #{text}"
   i += 1
+end
+
+puts " "
+puts "Example uses:"
+
+if example_info.parse != nil
+  examples = []
+  example_info.parse["examples"].each do |ex|
+    examples << {title: ex["title"], text: ex["text"]}
+  end
+
+
+  i = 1
+  examples.each do |ex|
+    puts "#{i}. (#{ex[:title]} - #{ex[:text]})"
+    i += 1
+  end
+else
+  puts "Sorry, no examples."  
 end
