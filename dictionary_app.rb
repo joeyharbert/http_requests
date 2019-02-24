ba0a62fba9bf1110e0bc6707474e8f4bb53f148a
joeyharbert <joey.harbert@gmail.com>
2019-02-24 09:39:57 -0600

    Fixes q to end loop

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -2,52 +2,59 @@ require 'http'
 
 apikey = ""
 
-puts "Welcome to Dictionary dot you're welcome. What do ya want to know bout?"
+puts "Welcome to Dictionary dot you're welcome. What do ya want to know bout? (enter q to quit)"
+query = ""
 
-query = gets.chomp.downcase
+while query != "q"
+  query = gets.chomp.downcase
 
-definition_info = HTTP.get("https://api.wordnik.com/v4/word.json/#{query}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{apikey}")
+  if query != "q"
 
-example_info = HTTP.get("https://api.wordnik.com/v4/word.json/#{query}/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=#{apikey}")
+    definition_info = HTTP.get("https://api.wordnik.com/v4/word.json/#{query}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=#{apikey}")
 
-pronunciation_info = HTTP.get("https://api.wordnik.com/v4/word.json/#{query}/pronunciations?useCanonical=false&limit=50&api_key=#{apikey}")
+    example_info = HTTP.get("https://api.wordnik.com/v4/word.json/#{query}/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=#{apikey}")
 
-word = definition_info.parse[0]["word"]
+    pronunciation_info = HTTP.get("https://api.wordnik.com/v4/word.json/#{query}/pronunciations?useCanonical=false&limit=50&api_key=#{apikey}")
 
-definition = []
+    word = definition_info.parse[0]["word"]
 
-definition_info.parse.each do |define|
-  definition << define["text"]
-end
+    definition = []
 
-pronunciation = pronunciation_info.parse[0]["raw"]
+    definition_info.parse.each do |define|
+      definition << define["text"]
+    end
+
+    pronunciation = pronunciation_info.parse[0]["raw"]
 
 
-part_of_speech = definition_info.parse[0]["partOfSpeech"]
+    part_of_speech = definition_info.parse[0]["partOfSpeech"]
 
-puts "#{word} (#{part_of_speech}): #{pronunciation}"
+    puts "#{word} (#{part_of_speech}): #{pronunciation}"
 
-i = 1
-definition.each do |text|
-  puts "#{i}. #{text}"
-  i += 1
-end
+    i = 1
+    definition.each do |text|
+      puts "#{i}. #{text}"
+      i += 1
+    end
 
-puts " "
-puts "Example uses:"
+    puts "\nExample uses:"
 
-if example_info.parse != nil
-  examples = []
-  example_info.parse["examples"].each do |ex|
-    examples << {title: ex["title"], text: ex["text"]}
+    if example_info.parse != nil
+      examples = []
+      example_info.parse["examples"].each do |ex|
+        examples << {title: ex["title"], text: ex["text"]}
+      end
+
+
+      i = 1
+      examples.each do |ex|
+        puts "#{i}. (#{ex[:title]}) - #{ex[:text]}"
+        i += 1
+      end
+    else
+      puts "Sorry, no examples."  
+    end
+
+    puts "\nAnything else?"
   end
-
-
-  i = 1
-  examples.each do |ex|
-    puts "#{i}. (#{ex[:title]} - #{ex[:text]})"
-    i += 1
-  end
-else
-  puts "Sorry, no examples."  
 end
